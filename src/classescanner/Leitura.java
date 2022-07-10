6e2863c1556563f5c8630603c99e042fcc84c0cc
Jose Rurze Batista de Almeida <jose.almeida@facid.edu.br>
2022-07-09 21:01:57 -0300

    first commit

---
+++ src/classescanner/Leitura.java
@@ -0,0 +1,17 @@
+package classescanner;
+
+import java.util.Scanner;
+
+public class Leitura {
+
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+		String leitura = ler.nextLine();
+					
+		
+		System.out.println("Escreve: "+leitura);
+		
+	}
+
+}
